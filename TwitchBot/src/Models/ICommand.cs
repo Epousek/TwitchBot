9587b3e780b70fa87a8ac848dd500ca83f7e8209
Epousek <epousek@seznam.cz>
2021-11-12 13:32:23 +0100

    create ICommand

---
+++ TwitchBot/src/Models/ICommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchBot.src.Models
+{
+  public interface ICommand
+  {
+    string Name { get; }
+    string About { get; }
+    string Help { get; }
+    int Permission { get; }
+    bool OfflineOnly { get; }
+    bool UsableByBanned { get; }
+    bool Optoutable { get; }
+    int TimesUsedSinceRestart { get; }
+    int? TimesUsedTotal { get; set; }
+
+    string GetAbout();
+    string GetHelp();
+  }
+}
