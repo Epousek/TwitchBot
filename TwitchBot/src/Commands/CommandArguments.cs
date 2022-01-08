5611c0993de9128676c26bf41242e2aad7442f22
Epousek <janbezouska@outlook.com>
2022-01-08 17:20:44 +0100

    methods for returning one or two arguments

---
+++ TwitchBot/src/Commands/CommandArguments.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TwitchBot.src.Models;
+
+namespace TwitchBot.src.Commands
+{
+  public class CommandArguments
+  {
+    readonly private string message;
+
+    public CommandArguments(ChatMessageModel m)
+    {
+      if (m.Message.Contains(' '))
+      {
+        message = m.Message.Remove(0, m.Message.IndexOf(' ') + 1); //remove the command
+        message = message.TrimStart();
+      }
+      else
+      {
+        message = string.Empty;
+      }
+    }
+
+    public List<string> GetOneArgument()
+    {
+      return new List<string>() { message };
+    }
+
+    public List<string> GetTwoArguments()
+    {
+      List<string> args = new List<string>(Regex.Split(message, "(?= )"));
+      int count = args.Count;
+      if (count < 2)
+        return new List<string>();
+      for (int i = 2; i < count; i++)
+      {
+        args[1] += args[2];
+        args.RemoveAt(2);
+      }
+
+      return args;
+    }
+  }
+}
