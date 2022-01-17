29db2dc8c72d913b7fb77c78fcf66c3f7f7ebc51
Epousek <epousek@seznam.cz>
2022-01-17 17:00:45 +0100

    create Reminder class

---
+++ TwitchBot/src/Models/Reminder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchBot.src.Models
+{
+  public class Reminder
+  {
+    public string Channel { get; set; }
+    public string From { get; set; }
+    public string For { get; set; }
+    public bool IsTimed { get; set; }
+    public DateTime StartTime { get; set; }
+    public DateTime? EndTime { get; set; }
+    public TimeSpan? Length { get; set; }
+    public int? ID { get; set; }
+  }
+}
