12fe51e6ac11587673c115aed3ccd04e5b8bdbad
Epousek <epousek@seznam.cz>
2022-01-18 15:10:48 +0100

    timed remind kinda working

--- TwitchBot/src/Commands/Remind.cs
+++ TwitchBot/src/Commands/Remind.cs
@@ -3,9 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Threading;
+using TwitchBot.src.Connections;
 using TwitchBot.src.Enums;
 using TwitchBot.src.Interfaces;
 using TwitchBot.src.Models;
+using System.Text;
+using Humanizer;
+using System.Globalization;
+using Humanizer.Localisation;
 
 namespace TwitchBot.src.Commands
 {
@@ -38,15 +44,73 @@ namespace TwitchBot.src.Commands
           if (reminder != null)
           {
             reminder.IsTimed = true;
-            reminder.For = args[0];
             reminder.From = message.Username;
+            reminder.For = string.Equals(args[0], "me") ? reminder.From : args[0];
             reminder.Channel = message.Channel;
+            reminder.Message = String.IsNullOrEmpty(args[3]) ? String.Empty : args[3];
+
+            var timer = new Timer(TimerElapsed, reminder, (int)reminder.Length.Value.TotalMilliseconds, Timeout.Infinite);
+            await DatabaseConnections.AddReminder(reminder).ConfigureAwait(false);
+
+            var builder = new StringBuilder('@');
+            builder
+              .Append(reminder.From)
+              .Append(' ');
+            if (string.Equals(reminder.For, reminder.From, StringComparison.OrdinalIgnoreCase))
+            {
+              builder
+                .Append("budeš upozorněn za ")
+                .Append(reminder.Length.Value.Humanize(3, new CultureInfo("cs-CS"), TimeUnit.Second));
+            }
+            else
+            {
+              builder
+                .Append(reminder.For)
+                .Append(" bude upozorněn za ")
+                .Append(reminder.Length.Value.Humanize(3, new CultureInfo("cs-CS"), TimeUnit.Second));
+            }
+            Bot.WriteMessage(builder.ToString(), message.Channel);
           }
         }
         {
 
         }
       }
+    }
+
+    private void TimerElapsed(object state)
+    {
+      var reminder = (Reminder)state;
+      var builder = new StringBuilder("@");
+      if (string.Equals(reminder.From, reminder.For, StringComparison.OrdinalIgnoreCase))
+      {
+        builder
+          .Append(reminder.For)
+          .Append(" Upozornění od tebe");
+      }
+      else
+      {
+        builder
+          .Append(reminder.For)
+          .Append(" Upozornění od ")
+          .Append(reminder.From);
+      }
+      if (string.IsNullOrEmpty(reminder.Message))
+      {
+        builder.Append(" bez zprávy. ");
+      }
+      else
+      {
+        builder
+          .Append(": ")
+          .Append(reminder.Message)
+          .Append(' ');
+      }
+      builder
+        .Append('(')
+        .Append((DateTime.Now - reminder.StartTime).Humanize(3, new CultureInfo("cs-CS"), minUnit: TimeUnit.Second))
+        .Append(')');
+      Bot.WriteMessage(builder.ToString(), reminder.Channel);
     }
 
     private void CheckTime(List<string> args, out Reminder reminder)
