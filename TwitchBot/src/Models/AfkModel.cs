9f62c8909b90d33a4a0322451f10e09bd947e9da
Epousek <epousek@seznam.cz>
2022-01-23 12:06:41 +0100

    created AfkModel

---
+++ TwitchBot/src/Models/AfkModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchBot.src.Models
+{
+  public class AfkModel
+  {
+    public string Channel { get; set; }
+    public string Username { get; set; }
+    public string Message { get; set; }
+    public DateTime AfkSince { get; set; }
+  }
+}
