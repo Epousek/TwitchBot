fd442556410617eb6d0874e6c33e56eb71501737
Epousek <epousek@seznam.cz>
2022-02-20 20:22:18 +0100

    add information about memory usage

--- TwitchBot/src/Commands/Info.cs
+++ TwitchBot/src/Commands/Info.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Globalization;
 using System.Text;
 using System.Threading.Tasks;
 using Humanizer;
+using Microsoft.Extensions.Primitives;
 using Octokit;
 using TwitchBot.Connections;
 using TwitchBot.Interfaces;
@@ -16,7 +18,7 @@ namespace TwitchBot.Commands
     public string Name { get; } = nameof(Info);
     public string AboutCommand { get; } = "Vypíše informace o právě běžící instanci bota.";
     public string HelpMessage { get; } = "$info";
-    public string[] Aliases { get; } = Array.Empty<string>();
+    public string[] Aliases { get; } = { "ping" };
     public Permission Permission { get; } = Permission.Regular;
     public TimeSpan Cooldown { get; } = TimeSpan.FromSeconds(10);
     public DateTime LastUsed { get; set; }
@@ -26,11 +28,14 @@ namespace TwitchBot.Commands
     public int TimesUsedSinceRestart { get; set; }
     public int? TimesUsedTotal { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 
+    private long _memoryUsage;
+
     public async Task UseCommandAsync(ChatMessageModel message)
     {
       var release = await GitHub.GetLastReleaseAsync();
       var totalCommandsUsed = await DatabaseConnections.GetTotalCommandsUsed().ConfigureAwait(false);
       var builder = new StringBuilder("@");
+      SetInformation();
       builder.Append(message.Username)
         .Append(" Uptime: ")
         .Append((DateTime.Now - BotInfo.RunningSince).Humanize(
@@ -39,7 +44,10 @@ namespace TwitchBot.Commands
           minUnit: Humanizer.Localisation.TimeUnit.Second))
         .Append("; počet použitých příkazů od zapnutí: ")
         .Append(BotInfo.CommandsUsedSinceStart)
-        .Append(totalCommandsUsed == -1 ? "" : $"; počet použitých příkazů celkem: {totalCommandsUsed}")
+        .Append(totalCommandsUsed == -1 ? "" : $"; použitých příkazů celkem: {totalCommandsUsed}")
+        .Append("; použitá paměť: ")
+        .Append(_memoryUsage)
+        .Append(" MB")
         .Append("; verze: ")
         .Append(release.TagName[1..])
         .Append(" (")
@@ -47,6 +55,14 @@ namespace TwitchBot.Commands
         .Append(')');
 
       Bot.WriteMessage(builder.ToString(), message.Channel);
+    }
+
+    private void SetInformation()
+    {
+      using (var process = Process.GetCurrentProcess())
+      {
+        _memoryUsage = process.PrivateMemorySize64 / 1024 / 1024;
+      }
     }
   }
 }
